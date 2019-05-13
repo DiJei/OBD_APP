faace455b8f00c93bc834c383382013b0f00fd98
DiJei <djrenjiffo@terra.com.br>
2019-05-12 21:16:34 -0300

    New activity Service, now file to add USER cars

--- app/src/main/java/com/zul/tests/obdzulbeta/LoginScreen.java
+++ app/src/main/java/com/zul/tests/obdzulbeta/LoginScreen.java
@@ -1,16 +1,18 @@
 package com.zul.tests.obdzulbeta;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -35,11 +37,11 @@ public class LoginScreen extends AppCompatActivity {
 
         String FILENAME = "listCars";
 
-        String  brandCar = brand.getText().toString();
-        String  modelCar = model.getText().toString();
-        String  engineCar = engine.getText().toString();
-        String  yearCar = year.getText().toString();
-        String  fuelTypeCar = fuelType.getText().toString();
+        String brandCar = brand.getText().toString();
+        String modelCar = model.getText().toString();
+        String engineCar = engine.getText().toString();
+        String yearCar = year.getText().toString();
+        String fuelTypeCar = fuelType.getText().toString();
 
         if (brandCar.length() < 1) {
             Toast.makeText(getApplicationContext(), "Digite todas as informações", Toast.LENGTH_LONG).show();
@@ -62,24 +64,36 @@ public class LoginScreen extends AppCompatActivity {
             return;
         }
 
-        String carLine = brandCar + "," + modelCar + "," + yearCar + "," + engineCar + "," +  fuelTypeCar + "\n";
+        String carLine = brandCar + "," + modelCar + "," + yearCar + "," + engineCar + "," + fuelTypeCar + "\n";
 
-        FileInputStream fis =  openFileInput(FILENAME);
+        String yourFilePath = this.getFilesDir() + "/" + FILENAME;
+        File yourFile = new File( yourFilePath );
+
+
+        if (yourFile.exists()) {
+            // do something
+
+
+        FileInputStream fis = openFileInput(FILENAME);
         InputStreamReader isr = new InputStreamReader(fis);
         BufferedReader bufferedReader = new BufferedReader(isr);
         String line;
         while ((line = bufferedReader.readLine()) != null) {
-            if (carLine.equals(line+"\n")) {
+            if (carLine.equals(line + "\n")) {
                 Toast.makeText(getApplicationContext(), "Já existente", Toast.LENGTH_LONG).show();
                 return;
             }
         }
+    }
 
         FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_APPEND);
         fos.write(carLine.getBytes());
         fos.close();
         Toast.makeText(getApplicationContext(), "Gravado", Toast.LENGTH_LONG).show();
         //Parte para tela de Serviços e conecta com OBD
+        Intent intent = new Intent(LoginScreen.this, ServiceActivity.class);
+        intent.putExtra("CAR_INFO",carLine);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/zul/tests/obdzulbeta/MainActivity.java
+++ app/src/main/java/com/zul/tests/obdzulbeta/MainActivity.java
@@ -3,6 +3,8 @@ package com.zul.tests.obdzulbeta;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -32,34 +35,54 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void showCarList(View view) throws IOException {
+    public void showCarList(View view) throws IOException, PackageManager.NameNotFoundException {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         final ArrayAdapter<String> mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1);
-        View row = getLayoutInflater().inflate(R.layout.car_list_view,null);
+        View row = getLayoutInflater().inflate(R.layout.car_list_view, null);
         ArrayList<String> listOfCars = new ArrayList<>();
 
 
-        ListView listCar = (ListView)row.findViewById(R.id.deviceList);
+        ListView listCar = (ListView) row.findViewById(R.id.deviceList);
         builder.setTitle("Escolha carro");
         String line;
 
-        FileInputStream fis =  openFileInput(FILENAME);
-        InputStreamReader isr = new InputStreamReader(fis);
-        BufferedReader bufferedReader = new BufferedReader(isr);
-        while ((line = bufferedReader.readLine()) != null)
-            mArrayAdapter.add(line);
-        listCar.setAdapter(mArrayAdapter);
 
+
+        String yourFilePath = this.getFilesDir() + "/" + FILENAME;
+        File yourFile = new File( yourFilePath );
+
+
+        if (yourFile.exists()) {
+
+            FileInputStream fis = openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            while ((line = bufferedReader.readLine()) != null)
+                mArrayAdapter.add(line);
+            listCar.setAdapter(mArrayAdapter);
+
+            if(mArrayAdapter.getCount() < 1) {
+                Toast.makeText(getApplicationContext(),"Sem usuários", Toast.LENGTH_LONG).show();
+                return;
+            }
 
         builder.setAdapter(mArrayAdapter, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
                 Toast.makeText(getApplicationContext(), mArrayAdapter.getItem(which), Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ServiceActivity.class);
+                intent.putExtra("CAR_INFO", mArrayAdapter.getItem(which));
+                startActivity(intent);
             }
         });
         AlertDialog alert = builder.create();
         alert.show();
+
+    }
+        else {
+            Toast.makeText(getApplicationContext(),"Sem usuários", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
